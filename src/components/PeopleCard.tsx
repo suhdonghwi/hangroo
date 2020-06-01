3988e80ef0b4b3bcc47461797a4f19a40ab46345
Suh Donghwi <hwidongsuh@gmail.com>
2020-06-01 11:40:33 +0900

    feat: add card title

--- src/components/PeopleCard.tsx
+++ src/components/PeopleCard.tsx
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 
 import colors from 'assets/colors';
 
-import Card from 'assets/styles/Card';
+import CardStyle from 'assets/styles/Card';
 
-interface PeopleCardProps {
+export interface PeopleCardProps {
   imageUrl: string;
   name: string;
   birth: number;
@@ -13,13 +13,31 @@ interface PeopleCardProps {
   description: string;
 }
 
+const Card = styled(CardStyle)`
+  padding: 1.75rem 1.5rem;
+  width: 250px;
+`;
+
 const CardImage = styled.img`
+  width: 230px;
+  height: 230px;
+
+  border-radius: 10px;
+  border: 2px solid ${colors.gray8};
+`;
+
+const CardTitle = styled.h1`
+  color: ${colors.gray7};
+  margin: 0.75rem 0;
+
+  font-size: 2rem;
 `;
 
 export default function PeopleCard(props: PeopleCardProps) {
   return (
     <Card>
       <CardImage src={props.imageUrl}/>
+      <CardTitle>{props.name}</CardTitle>
 
     </Card>
   );
--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 
 import NavBar from 'components/NavBar';
 
+import PeopleCard, {PeopleCardProps} from 'components/PeopleCard';
+
 const MainContainer = styled.main`
 
 `;
@@ -16,6 +18,13 @@ export default function PeoplePage() {
     <>
       <NavBar />
       <MainContainer>
+        <PeopleCard
+          imageUrl="/people/세종대왕.png"
+          name="세종대왕"
+          birth={1397}
+          death={1450}
+          description="조선의 제4대 국왕이자, 한글의 창제자이십니다. 한글을 제외해도 조선 최고의 국왕으로 자주 일컬어지며 존경의 의미로 '대왕'이라는 칭호로 불립니다."
+        />
       </MainContainer>
     </>
   );
