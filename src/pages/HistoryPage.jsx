3ed747e3cdbcc50d05946d9d123a4e567969deb0
Suh Donghwi <hwidongsuh@gmail.com>
2020-06-03 14:32:18 +0900

    feat: add section background properties

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -4,19 +4,27 @@ import ReactFullpage from '@fullpage/react-fullpage';
 import styled from 'styled-components';
 import NavBar from 'components/NavBar';
 
+const Section = styled.section`
+  background: 
+      linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.5)),
+      url('${props => props.imageUrl}');
+  background-size: cover;
+
+`;
+
 export default function HistoryPage() {
   const render = ({state, fullpageApi}) => {
     return (
       <ReactFullpage.Wrapper>
-        <div className="section">
+        <Section className="section" imageUrl="/history/한글 창제.jpg">
           <p>Section 1 (welcome to fullpage.js)</p>
           <button onClick={() => fullpageApi.moveSectionDown()}>
             Click me to move down
             </button>
-        </div>
-        <div className="section">
+        </Section>
+        <Section className="section">
           <p>Section 2</p>
-        </div>
+        </Section>
       </ReactFullpage.Wrapper>
     );
   };
