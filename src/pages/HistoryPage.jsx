0c1213c44241cb6f227eb415d737dc32f08bf293
Suh Donghwi <hwidongsuh@gmail.com>
2020-06-04 22:05:25 +0900

    feat: add more history pages

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -5,6 +5,8 @@ import AnimatedNumber from "animated-number-react";
 import styled from 'styled-components';
 import colors from 'assets/colors';
 
+import historyData from 'assets/HistoryData';
+
 const Section = styled.section`
   background: 
       linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
@@ -104,42 +106,15 @@ const Description = styled.p`
 `;
 
 export default function HistoryPage() {
-  const sectionProps = [
-    {
-      year: 1444,
-      imageUrl: '세종.png',
-      title: '한글을 창제하다',
-      description: '한글은 서기 1443년 12월 ~ 1444년 1월 중순 (세종 25년 12월) 경에 창제되었다. 정확한 창제 주체에 관해서는 논란이 있지만, 세종대왕 본인이 소수 최측근의 도움만을 받아 창제하였을 것이라는 설이 가장 유력하다.'
-    },
-    {
-      year: 1446,
-      imageUrl: '훈민정음.jpg',
-      title: '한글을 반포하다',
-      description: '세종은 직접 창제한 한글을 3년 동안 직접 써보면서 다듬고, 이를 1446년 9월 (세종 27년 경)에 반포하였다. 당시 반포한 훈민정음 해례본에는 한글을 창제한 과학적, 철학적 원리가 담겨있고, 현재까지 전해져 내려온다.'
-    },
-    {
-      year: 1447,
-      imageUrl: '용비어천가.jpg',
-      title: '한글로 책을 쓰다',
-      description: '세종은 한글을 시험해보기 위해 학자들을 시켜 1447년 (세종 28년) 경 용비어천가를 편찬했다. 한글로 쓰인 최초의 책이라는 의의가 있으며, 그만큼 초기 국어 및 한글의 특징을 제대로 담고있어 중요한 유산이다.'
-    },
-    {
-      year: 1504,
-      imageUrl: '연산군.jpg',
-      title: '한글이 탄압되다',
-      description: '1504년 (연산 10년) 연산군이 투서를 받았는데, 그 속에는 한글로 자신을 비방하는 글이 쓰여져있었다. 분노한 연산군은 한글을 사용하는 자는 체포하라는 명령을 내렸다. 하지만 그도 한글의 유용함을 잘 알고있었고, 정책은 오래가지 못했다.'
-    },
-  ];
-
-  const [year, setYear] = useState(sectionProps[0].year);
-  const [title, setTitle] = useState(sectionProps[0].title);
+  const [year, setYear] = useState(historyData[0].year);
+  const [title, setTitle] = useState(historyData[0].title);
   const [hideTitle, setHideTitle] = useState(false);
 
   const render = () => {
     return (
       <ReactFullpage.Wrapper>
         {
-          sectionProps.map(props => (
+          historyData.map(props => (
             <Section key={props.imageUrl} className="section" imageUrl={"/history/" + props.imageUrl}>
               <Description>
                 {props.description}
@@ -152,12 +127,12 @@ export default function HistoryPage() {
   };
 
   const onLeave = (_, dest) => {
-    setYear(sectionProps[dest.index].year);
+    setYear(historyData[dest.index].year);
     setHideTitle(true);
 
     setTimeout(() => {
       setHideTitle(false);
-      setTitle(sectionProps[dest.index].title);
+      setTitle(historyData[dest.index].title);
     }, 500);
   };
 
