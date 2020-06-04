8b400a316f8bf68c8083fb55f4b3b8694ade0b5b
Suh Donghwi <hwidongsuh@gmail.com>
2020-06-04 09:41:33 +0900

    feat: add another section to history page

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -56,17 +56,31 @@ const Description = styled.p`
 `;
 
 export default function HistoryPage() {
+  const sectionProps = [
+    {
+      year: 1444,
+      imageUrl: '한글 창제.jpg',
+      description: '한글은 서기 1443년 12월 ~ 1444년 1월 중순 (세종 25년 12월) 경에 창제되었다. 정확한 창제 주체에 관해서는 논란이 있지만, 세종대왕 본인이 소수 최측근의 도움만을 받아 창제하였을 것이라는 설이 가장 유력하다.'
+    },
+    {
+      year: 1446,
+      imageUrl: '반포.png',
+      description: '세종은 직접 창제한 한글을 3년 동안 직접 써보면서 다듬고, 이를 1446년 9월 (세종 27년 경)에 반포하였다. 당시 처음으로 반포한 훈민정음 해례본에는 한글을 창제한 과학적, 철학적 원리가 담겨있고, 현재까지 전해져 내려온다.'
+    },
+  ];
+
   const render = () => {
     return (
       <ReactFullpage.Wrapper>
-        <Section className="section" imageUrl="/history/한글 창제.jpg">
-          <Description>
-            한글은 서기 1443년 12월 ~ 1444년 1월 중순 (세종 25년 12월) 경에 창제되었다. 정확한 창제 주체에 관해서는 논란이 있지만, 세종대왕 본인이 소수 최측근의 도움만을 받아 창제하였을 것이라는 설이 가장 유력하다.
-            </Description>
-        </Section>
-        <Section className="section">
-          <p>Section 2</p>
-        </Section>
+        {
+          sectionProps.map(props => (
+            <Section className="section" imageUrl={"/history/" + props.imageUrl}>
+              <Description>
+                {props.description}
+              </Description>
+            </Section>
+          ))
+        }
       </ReactFullpage.Wrapper>
     );
   };
