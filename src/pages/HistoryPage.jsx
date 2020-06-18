76f88ad7bc5b40af532d6810f02a1e30459213b3
Suh Donghwi <hwidongsuh@gmail.com>
2020-06-18 14:33:26 +0900

    feat: add scroll down indicator in history page

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -18,6 +18,35 @@ const Section = styled.section`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  &:first-child {
+    &::before {
+      animation: bounce 1s ease infinite;
+      bottom: 2rem;
+      color: #fff;
+      content: '╲╱';
+      font-size: 1.5rem;
+      height: 4rem;
+      left: 50%;
+      letter-spacing: -1px;
+      line-height: 4rem;
+      margin-left: -3rem;
+      opacity: 0.8;
+      position: absolute;
+      text-align: center;
+      width: 6rem;
+    }
+
+    @keyframes bounce {
+      50% {
+        transform: translateY(-50%);
+      }
+
+      100% {
+        transform: translateY(0);
+      }
+    }
+  }
 `;
 
 const Year = styled.h1`
