4b20b3e783dcd9617d1c4b88ad74a678117d99b0
Suh Donghwi <hwidongsuh@gmail.com>
2020-06-23 14:29:19 +0900

    feat: make scroll down indicator not visible in small screens

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -19,31 +19,33 @@ const Section = styled.section`
   align-items: center;
   justify-content: center;
 
-  &:first-child {
-    &::before {
-      animation: bounce 1s ease infinite;
-      bottom: 2rem;
-      color: #fff;
-      content: '╲╱';
-      font-size: 1.5rem;
-      height: 4rem;
-      left: 50%;
-      letter-spacing: -1px;
-      line-height: 4rem;
-      margin-left: -3rem;
-      opacity: 0.8;
-      position: absolute;
-      text-align: center;
-      width: 6rem;
-    }
-
-    @keyframes bounce {
-      50% {
-        transform: translateY(-50%);
+  @media only screen and (min-height: 600px) {
+    &:first-child {
+      &::before {
+        animation: bounce 1s ease infinite;
+        bottom: 2rem;
+        color: #fff;
+        content: '╲╱';
+        font-size: 1.5rem;
+        height: 4rem;
+        left: 50%;
+        letter-spacing: -1px;
+        line-height: 4rem;
+        margin-left: -3rem;
+        opacity: 0.8;
+        position: absolute;
+        text-align: center;
+        width: 6rem;
       }
 
-      100% {
-        transform: translateY(0);
+      @keyframes bounce {
+        50% {
+          transform: translateY(-50%);
+        }
+
+        100% {
+          transform: translateY(0);
+        }
       }
     }
   }
